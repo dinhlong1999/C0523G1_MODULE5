c54eda742bb78a9d03fbf9de0949f82a50371e82
dinhlong1999 <long.truong.vcr@gmail.com>
2023-10-15 23:17:54 +0700

    longup

---
+++ furama/src/main/java/com/example/furama/Test.java
@@ -0,0 +1,13 @@
+package com.example.furama;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Test {
+
+    @GetMapping
+    public String test(){
+        return "index";
+    }
+}
